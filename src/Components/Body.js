d781fad8cadbdbba94e5b4c2858d8335b3185bd3
Ammarkhan227 <119163575+Ammarkhan227@users.noreply.github.com>
2024-02-23 23:42:04 +0530

    Filter Button implementation
    Here we have created an filter button that will filter our the top rated restaurants which have the rating of 4 or more than that using the useState function.

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,17 +1,33 @@
 import Card from './Card'
 import resList from '../utils/Data';
+import { useState } from "react";
+
+
 
 const Body = () => {
+    const [listofResturants, setlistofResturants] = useState(resList);
     return (
-        <div className='main'>
-            <div className='carddisplay'>
-                {
-                    resList.map(restaurant => <Card key={restaurant.info.id} restData={restaurant} />)
-                }
+        <div className='body'>
+
+            {/* Filter button  */}
+            <div className='resfilter'>
+                <button onClick={() => {
+                    const filterlist = listofResturants.filter((res) => res.info.avgRating > 4);
+                    setlistofResturants(filterlist);
+                }}>Top Rated Restaurant</button>
+            
+            {/* Resturant Cards */}
+            <div className='main'>
+                <div className='carddisplay'>
+                    {
+                        listofResturants.map((restaurant) => (<Card key={restaurant.info.id} restData={restaurant} />))
+                    }
+                </div>
             </div>
         </div>
-    )
-}
+      </div>  
+    );
+};
 
 
 export default Body;
